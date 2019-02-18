066e11c9df0c558f974c4318b797360687d78304
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-02-18 14:02:51 +0200

    finished product post add-product route  validation

--- routes/admin.js
+++ routes/admin.js
@@ -24,33 +24,96 @@ router.post(
           }
         });
       }),
-      body(
-        'brand',
-        ' Brand should be min 2 and max 4 characters long and should be alphabetic'
-      )
-        .isLength({ min: 2,max: 4 })
-        .isAlpha()
-        .isString()
-        .escape()
-        .trim(),
-        body(
-            'Price',
-            ' Brand should be atlest 2 characters long and should be alphabetic'
-          )
-            .isLength({ min: 3,max : 4 })
-            .isNumberic()
-            .isString()
-            .escape()
-            .trim(),
-            body(
-                'availableUnits',
-                ' Available units shpuld be biggner than 1'
-              )
-                .isLength({ min: 1,max : 4 })
-                .isNumberic()
-                .isString()
-                .escape()
-                .trim()
+    body(
+      'brand',
+      ' Brand should be min 2 and max 4 characters long and should be alphabetic'
+    )
+      .isLength({ min: 2, max: 4 })
+      .isAlpha()
+      .isString()
+      .escape()
+      .trim(),
+    body(
+      'Price',
+      ' Brand should be atlest 2 characters long and should be alphabetic'
+    )
+      .isLength({ min: 3, max: 4 })
+      .isNumeric()
+      .isString()
+      .escape()
+      .trim(),
+    body('availableUnits', ' Available units shpuld be biggner than 1')
+      .isLength({ min: 1, max: 4 })
+      .isNumeric()
+      .isString()
+      .escape()
+      .trim(),
+    body('screenSize', 'Screen size should be bigger than 12 inches')
+      .isLength({ min: 2, max: 2 })
+      .isNumeric()
+      .isString()
+      .escape()
+      .trim(),
+    body('cpu', 'CPU descruption should be longer than 10 characters.')
+      .isLength({ min: 10 })
+      .isAlphanumeric()
+      .isString()
+      .escape()
+      .trim(),
+    body('gpu', 'GPU descruption should be longer than 10 characters.')
+      .isLength({ min: 10 })
+      .isAlphanumeric()
+      .isString()
+      .escape()
+      .trim(),
+    body(
+      'ram',
+      'RAM value should be even number bigger than 4 and smaller than 32.'
+    )
+      .isLength({ min: 1, max: 2 })
+      .isNumeric()
+      .isString()
+      .escape()
+      .trim()
+      .custom((ram, { req }) => {
+        if (ram % 2 !== 0 || ram > 32 || ram < 4) {
+          return Promise.reject('RAM Value is invalid');
+        }
+      }),
+    body('memoryType', 'Memory type should be either SSD or HDD.')
+      .isLength({ min: 3, max: 3 })
+      .isAlpha()
+      .isString()
+      .escape()
+      .trim()
+      .custom((memoryType, { req }) => {
+          if (memoryType !== "ssd" || memoryType !== "hdd" ) {
+            return Promise.reject('Memory Type value is invalid');
+          }
+      }),
+      body('capacity', 'Memory capacity  should be either  smaller than 1000 and biggner than 250')
+      .isLength({ min: 3, max: 4 })
+      .isNumeric()
+      .isString()
+      .escape()
+      .trim()
+      .custom((capacity, { req }) => {
+          if (capacity > 1000 || capacity < 250) {
+            return Promise.reject('Capacity  value is invalid');
+          }
+      }),
+      body('batteryLifre', 'Description of battery life')
+      .isLength({ min: 1, max:2  })
+      .isAlpha()
+      .isString()
+      .escape()
+      .trim(),
+      body('description', 'Description must be atleast 30 characters')
+      .isLength({ min: 1, max:2  })
+      .isAlphanumeric()
+      .isString()
+      .escape()
+      .trim()
   ],
   isAdmin,
   adminController.postAddProduct
