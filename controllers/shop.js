a4a1a977dab2ad446e5eb44ef3a97b54d9a1300c
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-02-20 17:57:12 +0200

    added filtering by brand the shop page

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,47 +1,52 @@
-const Product = require('../models/product')
-exports.getShop = async (req,res,next) => {
-    try{
-        const products = await Product.find()
-        res.render('shop/shop', {
-            products,
-            title:'Shop',
-            path: '/shop',
-            errorMessage:false,
-          })
-    }catch(err){
-        
+const Product = require('../models/product');
+exports.getShop = async (req, res, next) => {
+  try {
+    const { brand } = req.query;
+    let products;
+    if (brand) {
+        console.log(brand)
+      products = await Product.find({brand});
+      console.log(products)
+    } else {
+      products = await Product.find();
     }
-   
-}
 
+    res.render('shop/shop', {
+      products,
+      title: 'Shop',
+      path: '/shop',
+      errorMessage: false
+    });
+  } catch (err) {}
+};
 exports.getProduct = async (req, res, next) => {
-    try {
-      const { productId } = req.params;
-      const product = await Product.findById(productId);
-      //Product specs with be array of arrays each containing name and value of product specifications.[[spec1:'dummy value'],[spec2:'dummy value']]
-      const productSpecs = Object.entries(product._doc).map(spec => {
-        const specName = spec[0];
-        const specValue = spec[1];
-        if (
-          specName !== 'description' &&
-          specName !== 'imageUrl' &&
-          specName !== '_id' &&
-          specName !== '__v' &&
-          specName !== 'availableUnits'
-        ) {
-          if (specName == 'memory') {
-            return [specName, `${specValue.memoryType} ${specValue.capacity}`];
-          }
-          return spec;
+  try {
+    const { productId } = req.params;
+    const product = await Product.findById(productId);
+    //Product specs with be array of arrays each containing name and value of product specifications.[[spec1:'dummy value'],[spec2:'dummy value']]
+    const productSpecs = Object.entries(product._doc).map(spec => {
+      const specName = spec[0];
+      const specValue = spec[1];
+      if (
+        specName !== 'description' &&
+        specName !== 'imageUrl' &&
+        specName !== '_id' &&
+        specName !== '__v' &&
+        specName !== 'availableUnits'
+      ) {
+        if (specName == 'memory') {
+          return [specName, `${specValue.memoryType} ${specValue.capacity}`];
         }
-      });
-  
-      res.render('product/product-details', {
-        product,
-        productSpecs,
-        title: product.name,
-        path: 'admin/product',
-        errorMessage: false
-      });
-    } catch (err) {}
-  };
+        return spec;
+      }
+    });
+
+    res.render('product/product-details', {
+      product,
+      productSpecs,
+      title: product.name,
+      path: 'admin/product',
+      errorMessage: false
+    });
+  } catch (err) {}
+};
